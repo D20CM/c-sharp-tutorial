4bf08875fbe854233eb9fa6d3a98f9297d033217
D20CM <scottbrown500@hotmail.com>
2022-05-22 12:17:13 +0100

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+        }
+
+        public bool IsChunky()
+        {
+            if (pages >= 500)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string phrase = "boom!";
+            char grade = 'A';
+            int age = 21;
+            double score = 3.2;
+            bool isCool = true;
+
+             
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+
+            Book book1 = new Book("The Hitchhiker's Guide To The Galaxy", "Douglas Adams",571);
+         
+            Book book2 = new Book("The Bear Comes Home", "Rafi Zaboor",298);
+
+            Console.WriteLine(book1.IsChunky());
+            Console.WriteLine(book2.IsChunky());
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
